ed93ceba756500eabefa0fe6773d495df4b2a746
ehachi <ehajianto@gmail.com>
2023-03-21 08:42:55 +0300

    DONE

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
-end
+    def index
+      items = Item.all.map { |item| item.as_json.merge(user: item.user.as_json(only: [:id, :username, :city])) }
+      render json: items
+    end
+  end
+  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
-end
+    def show
+      user = User.find(params[:id])
+      items = user.items.as_json(only: [:id, :name, :description, :price])
+      render json: user.as_json(only: [:id, :username, :city]).merge(items: items)
+    end
+  end
+  
